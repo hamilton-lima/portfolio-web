2e978e73ff8932677de10f73a3d47412486e735a
Hamilton Lima <hamilton.lima@gmail.com>
2018-06-29 12:57:54 -0400

    Add title to links

--- src/app/person.ts
+++ src/app/person.ts
@@ -6,6 +6,7 @@ export class Person {
 }
 
 export class Site {
+    title: string;
     type: string;
     url: string;
 }
---
+++ src/app/project.ts
@@ -0,0 +1,11 @@
+export class Project {
+    title: string;
+    image: string;
+    description: string;
+    
+    url: string;
+    tags: string[];
+    startYear: string;
+    endYear: string;
+    company: string;
+}
