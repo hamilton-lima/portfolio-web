6cd8f7bc2da45d0ac11962cb6c6661b049176d9c
Hamilton Lima <hamilton.lima@gmail.com>
2018-06-30 13:42:42 -0400

    Checks if node_modules are installed

--- build.sh
+++ build.sh
@@ -9,6 +9,14 @@ else
     SITENAME=$1
 fi
 
+echo Check if node_modules is up to date
+NODEMODULE_FILES=$(ls -l ./node_modules | wc -l)
+echo files=$NODEMODULE_FILES
+
+if [[ $NODEMODULE_FILES -eq 0 ]]; then
+     npm install
+fi
+
 echo Updating index.html
 ./node_modules/.bin/handlebars $SITENAME/person.json < src/index.hbs > src/index.html
 
