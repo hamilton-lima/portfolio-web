03264af6b352b39238aa45cb3b43d30d28a7e412
Hamilton Lima <hamilton.lima@gmail.com>
2018-07-01 10:09:00 -0400

    Changes build script to use YML format as input

--- build.sh
+++ build.sh
@@ -11,7 +11,6 @@ fi
 
 echo Check if node_modules is up to date
 NODEMODULE_FILES=$(ls -l ./node_modules | wc -l)
-echo files=$NODEMODULE_FILES
 
 if [[ $NODEMODULE_FILES -eq 0 ]]; then
      npm install
@@ -20,15 +19,24 @@ fi
 echo Updating index.html
 ./node_modules/.bin/handlebars $SITENAME/person.json < src/index.hbs > src/index.html
 
-echo Copying files from $SITENAME
-
-set -e
-set -o xtrace
+echo Copying favicon.ico from $SITENAME
 cp $SITENAME/favicon.ico src/
-cp $SITENAME/theme.json src/assets
-cp $SITENAME/person.json src/assets
-cp $SITENAME/projects.json src/assets
 
+echo Generating theme.json from $SITENAME/theme.yml
+./node_modules/.bin/yaml2json $SITENAME/theme.yml > src/assets/theme.json
+
+echo Generating person.json from $SITENAME/person.yml
+./node_modules/.bin/yaml2json $SITENAME/person.yml > src/assets/person.json
+
+echo Generating projects.json from $SITENAME/projects.yml
+./node_modules/.bin/yaml2json $SITENAME/projects.yml > src/assets/projects.json
+
+echo Building the Angular web application to dist/portfolio-web
 ng build --prod
 
-echo 'The generated website is saved at the folder /dist'
+echo 
+./node_modules/.bin/vaca
+echo
+echo The generated website is saved at the folder /dist/portfolio-web
+echo 
+echo Have a $(./node_modules/.bin/superb) day.
